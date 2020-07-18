d6f00c4491015a98f9cd78920fc6c6b07a6c7d2f
Anasu <misushima@gmail.com>
2020-07-18 11:41:08 -0400

    Se inicia el proyecto

--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//import Bootstrap y BS CSS
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Usar Bootstrap Vue en el proyecto
+Vue.use(BootstrapVue)
+// Optional. Permite utilizar los íconos de bootstrap
+Vue.use(IconsPlugin)
+
+//iconos de font awesome para RRSS
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faTwitter, faGithub} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// poner el mismo nombre del ícono(s) importado(s) más arriba
+library.add(faFacebook)
+library.add(faTwitter)
+library.add(faGithub)
+
+// inicializa la componente de vue de font awesome
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
