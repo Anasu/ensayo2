5f27af9c88c1cc86bd1c7f0079f6ff8289c88b8e
Anasu <misushima@gmail.com>
2020-07-18 15:22:21 -0400

    Se agregan funcionalidades de traer tarjetas y modal

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { apiCall } from '../config/apiConfig.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    characters: [],
+    comentarios: [],
+  },
+  getters: {
+    getCharacters(state)
+    {
+      return state.characters;
+    }
   },
   mutations: {
+    FILLCHAR(state, apiRes)
+    {
+      state.characters = apiRes;
+    },
+    SAVECOMMENT(state, obj)
+    {
+      state.comentarios.push(obj);
+    }
+      
   },
   actions: {
+    fillChar(context)
+    {
+      let char = `character/`;
+
+      async function apiRes()
+      {
+        const apiARR = await (
+          await apiCall(char)
+        ).data.results;
+        
+        context.commit('FILLCHAR', apiARR)
+      }
+
+      apiRes();
+    },
+    saveComment(context, comentObj)
+    {
+      context.commit('SAVECOMMENT', comentObj)
+    }
   },
   modules: {
   }
