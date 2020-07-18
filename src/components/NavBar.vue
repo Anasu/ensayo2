711ed718451e023d0b31ee9c97759e7e356934e9
Anasu <misushima@gmail.com>
2020-07-18 11:41:46 -0400

    Se importa navbar de otro proyecto

---
+++ src/components/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <!-- Navbar con logo e items , Artículos y Admin -->
+    <b-navbar toggleable="lg" type="dark" variant="danger">
+      <!-- Logo -->
+      <b-navbar-brand href="#">
+        <router-link 
+          :to="{name: 'Artistas'}" 
+          class="navbar_title"
+        >Actual Music</router-link>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Items alineados a la derecha -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item right href="#">
+            <router-link 
+              :to="{name: 'Artistas'}" 
+              class="navbar_item-blanco"
+            >Artistas</router-link>
+          </b-nav-item>
+          <b-nav-item right href="#">
+            <router-link 
+              :to="{name: 'Articulos'}"
+              class="navbar_item-blanco"
+            >Artículos</router-link>
+          </b-nav-item>
+          <b-nav-item right href="#">
+            <router-link 
+              :to="{name: 'Admin'}"
+              class="navbar_item-blanco"
+            >Administración</router-link>
+          </b-nav-item>
+
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.navbar_item-blanco {
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-size: 1rem;
+
+  &:hover {
+    text-decoration: underline;
+    color: white;
+  }
+}
+
+.navbar_title {
+  text-decoration: none;
+  color: white;
+  font-size: 1.25rem;
+
+  &:hover {
+    font-weight: bold;
+    color: white;
+  }
+}
+</style>
